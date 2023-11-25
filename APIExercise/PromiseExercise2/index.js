a1862df50e7c4c4200b81f5d18bc1d243830b12f
Md-razvi <mraza1748@gmail.com>
2023-11-25 19:05:23 +0530

    Promises Comitted

--- APIExercise/PromiseExercise2/index.js
+++ APIExercise/PromiseExercise2/index.js
@@ -1,8 +1,8 @@
-
-
 let butt=document.querySelector(".clickDis");
 let butt2=document.querySelector('.btn2');
 let table=document.querySelector('table');
+let butt3=document.querySelector('.btn');
+
 butt2.addEventListener('click',(e)=>{
     apiCall(true);
 })
@@ -11,6 +11,8 @@ butt.addEventListener('click',(e)=>{
     apiCall(false)
     // console.log(b)
 })
+
+
 function apiCall(bool){
     // let data1;
     fetch('https://restcountries.com/v2/all').then(data=>{
@@ -38,10 +40,10 @@ table.innerHTML=`<thead>
 <tr>
 <th>Country</th>
 <th>Capital</th>
-<th>Currency</th>
+<th>Population</th>
 </tr>
 </thead>
-<tbody >
+<tbody>
 
 </tbody>`
 let tb=document.querySelector('tbody');
@@ -50,7 +52,7 @@ let arr1=arr.map((el)=>{
      let a=el.languages[0].name
      let item=`<td>${el.name}</td>
      <td>${el.capital}</td>
-     <td>${a}</td>
+     <td>${el.population}</td>
      `;
 
      return `<tr>${item}</tr>`
@@ -61,4 +63,116 @@ let arr1=arr.map((el)=>{
 
 // </tr>`
 
+}
+
+function SortFunction(arr,n){
+    
+ for(let i=0;i<n-1;i++){
+    // let isSwaptrue=false;
+    for(let j=0;j<n-i-1;j++)
+    {
+        if(arr[j]?.area<arr[j+1]?.area)
+        {
+            swap(arr,i,j);
+            // isSwaptrue=true;
+        }
+
+
+    }
+    // if(isSwaptrue==false)
+    // {
+    //     break
+    // }
+ }   
+}
+
+function swap(arr,i,j){
+    let temp;
+    temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+function Display2(arr){
+    table.innerHTML=`<thead>
+    <tr>
+    <th class="heads" >Index No</th>
+    <th class="heads" >Country</th>
+    <th class="heads" >Population</th>
+    </tr>
+    </thead>
+    <tbody >
+    
+    </tbody>`
+    let tb=document.querySelector('tbody');
+    let arr1=[];
+    for(let i=0;i<10;i++)
+    {
+        let item=`
+        <td class="data2">${i+1}.</td>
+        <td class="data2">${arr[i].name}</td>
+        <td class="data2">${arr[i].population}</td>
+        `;
+        arr1.push(`<tr>${item}</tr>`)   
+    }
+    // let arr1=arr.map((el)=>
+    // {
+     
+    //  return `<tr>${item}</tr>`
+    // })
+
+     tb.innerHTML=arr1.join('');
+}
+butt3.addEventListener('click',function(e){
+   fetch('https://restcountries.com/v2/all').then(data=>{
+    console.log(data)
+    return data.json()
+   }).then((Data)=>{
+    // console.log(Data)
+    SortFunction(Data,Data.length)
+    console.log(Data)
+    Display2(Data)
+   })
+})
+// Create another Button to read the
+// countries api and count total number of
+// languages in the world used as officials and
+// display them.
+let butt4=document.querySelector('.btn1');
+butt4.addEventListener('click',function(e){
+    fetch('https://restcountries.com/v2/all').then(data=>{
+        // console.log(data)
+        return data.json()
+    }).then((data1)=>{
+        
+        // console.log(data1);
+        Display4(data1)
+    })
+})
+
+function Display4(arr){
+ table.innerHTML=null;
+ table.innerHTML=`<thead>
+    <th class='heads'>Country</th>
+    <th class="heads">No. Of Languages</th>
+    <th class="heads">Languages</th>
+    </thead>
+    <tbody>
+    </tbody>
+    `
+ let tb=document.querySelector('tbody');
+ let arr1=arr.map((el)=>{
+    let arr2=el.languages.reduce((acc,item)=>{
+        acc.push(item.name);
+        return acc
+    },[])
+    let item=`<td class='data2'>${el.name}</td>
+    <td class='data2'>${arr2.length}</td>
+    <td class='data2'>${arr2}</td>
+    `
+    
+    return `<tr>${item}</tr>`
+ })
+
+ tb.innerHTML=arr1.join("");
+ 
 }
